82114a60cb98f6982ac51e77f36310d3f60f2e34
gwpjp <john.pearson.02@gmail.com>
2017-10-25 08:54:38 -0400

    Added skeleton code.

---
+++ sum35.js
@@ -0,0 +1,46 @@
+// The input to sum35 is a number.
+// The function outputs the sum of all multiples of 3 and 5 that are less than that number.
+
+function sum35(N) {
+
+}
+
+sum35(6);
+sum35(20);
+sum35(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------
+// Testing - DON'T CHANGE ANYTHING BELOW
+const printme = console.log;
+const out = [];
+console.log = function log(d) {
+    out.push(d);
+};
+const N = 30;
+sum35(N);
+printme('\nTesting...');
+printme(`\t did you print the proper number of multiples?\t ${out[0] == 'There are 13 multiples of 3 and 5 that are less than 30.'} `);
+printme(`\t did you print the proper sum?\t ${out[1] == 'Their sum is 195.'} `);

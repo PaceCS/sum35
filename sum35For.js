d96d0348a046a17054bf7ce7f88e261177a19fe1
gwpjp <john.pearson.02@gmail.com>
2017-10-25 08:59:14 -0400

    Created skeleton code with For and While.

---
+++ sum35For.js
@@ -0,0 +1,46 @@
+// The input to sum35For is a number.
+// The function outputs the sum of all multiples of 3 and 5 that are less than that number.
+
+function sum35For(N) {
+
+}
+
+sum35For(6);
+sum35For(20);
+sum35For(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------
+// Testing - DON'T CHANGE ANYTHING BELOW
+const printme = console.log;
+const out = [];
+console.log = function log(d) {
+    out.push(d);
+};
+const N = 30;
+sum35For(N);
+printme('\nTesting...');
+printme(`\t did you print the proper number of multiples?\t ${out[0] == 'There are 13 multiples of 3 and 5 that are less than 30.'} `);
+printme(`\t did you print the proper sum?\t ${out[1] == 'Their sum is 195.'} `);
--- sum35While.js
+++ sum35While.js
@@ -1,13 +1,13 @@
-// The input to sum35 is a number.
+// The input to sum35While is a number.
 // The function outputs the sum of all multiples of 3 and 5 that are less than that number.
 
-function sum35(N) {
+function sum35While(N) {
 
 }
 
-sum35(6);
-sum35(20);
-sum35(1000);
+sum35While(6);
+sum35While(20);
+sum35While(1000);
 
 
 
@@ -40,7 +40,7 @@ console.log = function log(d) {
     out.push(d);
 };
 const N = 30;
-sum35(N);
+sum35While(N);
 printme('\nTesting...');
 printme(`\t did you print the proper number of multiples?\t ${out[0] == 'There are 13 multiples of 3 and 5 that are less than 30.'} `);
 printme(`\t did you print the proper sum?\t ${out[1] == 'Their sum is 195.'} `);
